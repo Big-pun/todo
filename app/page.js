ddf94987626df948ae5507096442e4bcc104243c
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-22 14:30:54 -0400

    function todolist

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 // Todo import all the components from components folder
 import React from "react";
 import Form from "@/components/Form";
@@ -8,12 +10,29 @@ import TODOList from "@/components/TODOList";
 // Todo create a functional component Page
 
 function Home() {
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+// Todo create a variable to store the number of completed todos
+  const todos_completed = todos.filter(
+    (todo) => todo.is_completed === true
+  ).length;
+
+  const total_todos = todos.length;
+
+// Todo return the Page component
   return (
     <div className="wrapper">
       <Header />
-      <TODOHero todos_completed={0} total_todos={0} />
-      <Form />
-      <TODOList todos={[]} />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+      <Form todos={todos} setTodos={setTodos}/>
+      <TODOList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
