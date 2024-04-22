82bb43ad3b0a8395efdd0eafab2de31efc6f5a98
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-22 08:51:12 -0400

    TodoHero

---
+++ components/TODOHero.jsx
@@ -0,0 +1,18 @@
+// Todo create function component called TODOHero
+function TODOHero({todos_completed, total_todos}) {
+    return (
+        <section>
+            <div>
+                <p>Task Done</p>
+                <p>Keep it up</p>
+            </div>
+            <div>
+                {todos_completed}/{total_todos}
+            </div>
+        </section>
+    );
+}
+// Todo export the TODOHero component
+
+export default TODOHero;
+
