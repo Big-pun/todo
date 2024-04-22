33a273675902fca0e372277a601daf546c16891e
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-22 09:04:20 -0400

    Create list component

---
+++ components/TODOList.jsx
@@ -0,0 +1,7 @@
+// Todo create list component
+
+function TODOList() {
+    return <ol className="todo_list"> {/* <li> list goes here */}</ol>;
+}
+// Todo export the list component
+export default TODOList;
