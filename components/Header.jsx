58ddd9822d00f0bba5f76dd0ecf181071658057e
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-22 08:43:46 -0400

    Creation du header

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+// todo create a new file called Header.jsx in the components folder
+// todo create a functional component called Header
+function Header() {
+    return (
+        <>  
+        <svg>
+            <path d="" />
+        </svg>
+        <h1>Todo App</h1>
+        </>
+    );
+}
+
+// todo export the Header component
+export default Header;
