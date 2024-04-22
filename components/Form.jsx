1618500255b95091e833a5d4039856c931eea637
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-22 09:00:49 -0400

    Form element

---
+++ components/Form.jsx
@@ -0,0 +1,28 @@
+// Todo create the form component
+function Form() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //reset the form
+        event.target.reset();
+    };
+return (
+    <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="todo">
+            <input
+                type="text"
+                id="todo"
+                name="todo"
+                placeholder="Write your next task here"
+            />
+        </label>
+        <button>
+            <span className="visually-hidden">Submit</span>
+            <svg>
+                <path d="" />
+            </svg>
+        </button>
+    </form>
+);
+}
+// Todo export the Form component
+export default Form;
